167ff7e29fe42b0fce5a22097777e1bd56d70af4
Martin Brunzel <martin.brunzel@gmx.de>
2022-12-05 20:10:36 +0100

    add to general utilities: intify (toInt(), space insensitive) and cutString (String to array<String> with same length)

--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,19 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Cut string into array of Strings, each of equal length len.
+ */
+fun cutString(text: String, len: Int): MutableList<String> {
+    val list = mutableListOf<String>()
+    for (i in text.indices step len) {
+        list.add(text.substring(i, minOf( i + len, text.length)))
+    }
+    return list
+}
+
+/**
+ * Removes spaces around string and converts to Int.
+ */
+fun intify(text: String): Int = text.trim().toInt()
